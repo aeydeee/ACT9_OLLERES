0a3121bc59ea7f5c0e966174d5371803ef1f9f10
aeydeee <aeydeee@gmail.com>
2024-04-24 17:18:41 +0800

    added a pagination for my index blade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,13 +7,19 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = 'Product List form in ProductController';
-//        return Product::all();
-        return view('products.index', [
-            'products' => $products
-        ]);
+        $keyword = $request->get('search');
+        $perPage = 5;
+        if (!empty($keyword)) {
+            $products = Product::where('name', 'LIKE', "%$keyword%")
+                ->orWhere('category', 'LIKE', "%$keyword%")
+                ->latest()->paginate($perPage);
+        } else {
+            $products = Product::latest()->paginate($perPage);
+        }
+
+        return view('products.index', ['products' => $products])->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     public function create()
@@ -23,44 +29,31 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'name' => ['nullable'],
-            'description' => ['nullable'],
-            'category' => ['nullable'],
-            'quantity' => ['nullable', 'integer'],
-            'price' => ['nullable', 'integer'],
-            'image' => ['nullable'],
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'category' => 'required|string|max:255',
+            'quantity' => 'required|integer|min:0',
+            'price' => 'required|numeric|min:0',
         ]);
 
-        return Product::create($data);
+        $product = new Product;
+        $file_name = time() . '.' . $request->image->getClientOriginalExtension();
+        $request->image->move(public_path('images'), $file_name);
+
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+        $product->save();
+
+        return redirect()->route('products.index')->with('success', 'Product Added Successfully');
     }
 
-
-    public function show(Product $product)
-    {
-        return $product;
-    }
-
-    public function update(Request $request, Product $product)
-    {
-        $data = $request->validate([
-            'name' => ['nullable'],
-            'description' => ['nullable'],
-            'category' => ['nullable'],
-            'quantity' => ['nullable', 'integer'],
-            'price' => ['nullable', 'integer'],
-            'image' => ['nullable'],
-        ]);
-
-        $product->update($data);
-
-        return $product;
-    }
-
-    public function destroy(Product $product)
-    {
-        $product->delete();
-
-        return response()->json();
-    }
 }
